467b410754199f76ac51c28a84f0bf9563329c90
Md. Fahim Ferdous <107836166+fahim-it-335@users.noreply.github.com>
2023-09-06 11:07:42 +0600

    Delete Profile directory

--- Profile/fahimapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from fahimapp.models import Contract
-
-# Register your models here.
-admin.site.register(Contract)
--- Profile/fahimapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Contract(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
-    subject = models.CharField(max_length=100)
-    message = models.TextField()
